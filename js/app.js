a3bf1023b02c62b964ec90789838c54af74bbba7
Lameck Matare <lameckfarai@gmail.com>
2017-03-15 10:01:43 +0200

    Layed out the app structure

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const questions = {
+  1: {
+    question: "What is git?",
+    answers: [
+      {label: "a", option: "version control softaware"},
+      {label: "b", option: "text editor"},
+      {label: "c", option: "blogging website"}
+    ],
+    answer: "a"
+  },
+  2: {
+    question: "What is github?",
+    answers: [
+      {label: "a", option: "dating website"},
+      {label: "b", option: "blogging website"},
+      {label: "c", option: "none of the above"}
+    ],
+    answer: "c"
+  },
+  3: {
+    question: "What are the advantages of using git&github?",
+    answers: [
+      {label: "a", option: "enables code sharing"},
+      {label: "b", option: "helps connect with friends and families"},
+      {label: "c", option: "helps getting dates"}
+    ],
+    answer: "a"
+  }
+}
